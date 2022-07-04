99ee9a55d763d4a5989bfa0f3b40f41e3edf8a05
Daoud Merchant <77672258+daoudmerchant@users.noreply.github.com>
2022-07-04 20:59:05 +0200

    Add loading component

---
+++ client/src/components/Loading.tsx
@@ -0,0 +1,35 @@
+import styled from "styled-components"
+
+import lp from "../assets/otherIcons/lp-freepik.png";
+
+const LoaderContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+const Loader = styled.img`
+    width: 60%;
+    filter: invert(1);
+    animation-name: spin;
+    animation-duration: 1000ms;
+    animation-iteration-count: infinite;
+    animation-timing-function: linear;
+    @keyframes spin { 
+        from { 
+            transform: rotate(0deg); 
+        } to { 
+            transform: rotate(360deg); 
+        }
+    }
+`
+
+const Loading = () => (
+    <LoaderContainer>
+        <Loader src={lp} alt="a spinning record"/>
+    </LoaderContainer>
+)
+
+export default Loading;
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -4,6 +4,7 @@ import { selectUser } from "../features/user/userSlice";
 import { getUserAccessToken } from "../features/user/userSlice";
 
 import Main from "./Main";
+import Loading from "../components/Loading";
 
 const Home = () => {
     const { code, state, error } = Object.fromEntries(new URLSearchParams(window.location.search).entries());
@@ -17,9 +18,8 @@ const Home = () => {
         }
         dispatch(getUserAccessToken({ code, state }))
     }, [code, state, status, token, dispatch])
-
     if (status === "loading") {
-        return <h1>Loading...</h1>
+        return <Loading/>;
     }
     if (error) {
         return <p>There was an error signing in: {error}</p>
